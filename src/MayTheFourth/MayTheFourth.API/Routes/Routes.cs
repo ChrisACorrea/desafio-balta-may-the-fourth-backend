0162fa758cbc43aa2b37871d61155662e61656b1
Jonathan Vale <creativeweb.de@gmail.com>
2024-04-27 21:06:13 -0300

    Adjust in swagger for production

--- src/MayTheFourth/MayTheFourth.API/Routes/Routes.cs
+++ src/MayTheFourth/MayTheFourth.API/Routes/Routes.cs
@@ -1,9 +1,18 @@
-﻿namespace MayTheFourth.API.Routes;
+﻿using Microsoft.AspNetCore.Http.HttpResults;
+
+namespace MayTheFourth.API.Routes;
 
 public static class Routes
 {
     public static void MapRoutes(this IEndpointRouteBuilder app)
     {
+        app.MapGet("/", context =>
+        {
+            context.Response.Redirect("/swagger/");
+            return Task.CompletedTask;
+        });
+
         app.MapCharactersRoutes();
+        app.MapMoviesRoutes();
     }
 }
