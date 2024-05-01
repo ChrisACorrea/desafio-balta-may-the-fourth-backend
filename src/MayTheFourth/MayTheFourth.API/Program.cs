1f18142b82d9b651a29f1740fedc0a825f03d0e3
Jonathan Vale <creativeweb.de@gmail.com>
2024-05-01 19:37:38 -0300

    Cleanup for deploy

--- src/MayTheFourth/MayTheFourth.API/Program.cs
+++ src/MayTheFourth/MayTheFourth.API/Program.cs
@@ -1,23 +1,27 @@
 using AutoMapper;
-using MayTheFourth.API.Helpers;
 using MayTheFourth.API.Routes;
-using MayTheFourth.Entities;
 using MayTheFourth.IoC;
-using MayTheFourth.Services.Interfaces;
 using MayTheFourth.Services.Mappers;
-using MayTheFourth.Services.ViewModels;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.OpenApi.Models;
 using System.Text.Json.Serialization;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(c =>
 {
-    c.SwaggerDoc("v1", new OpenApiInfo { Title = "MayTheFourth", Version = "v1" });
+    c.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "MayTheFourth", 
+        Description = "Developed by Christopher Corrêa | Gabriel Ferreira | Jonathan Vale | Gabriel | André",
+        Contact = new OpenApiContact
+        {
+            Name = "MayTheFourth",
+            Url = new Uri("https://github.com/KennyMack/desafio-balta-may-the-fourth-backend")
+        },
+        
+        Version = "v1"
+    });
 });
 
 builder.Services.AddServices(builder.Configuration);
@@ -28,6 +32,18 @@ builder.Services.ConfigureHttpJsonOptions(options =>
     options.SerializerOptions.IncludeFields = true;
     options.SerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles;
 });
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("MayTheFourth", cors =>
+    {
+        cors.AllowAnyOrigin()
+            .AllowAnyMethod()
+            .AllowAnyHeader();
+    });
+});
+
+builder.Services.AddMemoryCache();
 
 var app = builder.Build();
 
@@ -40,6 +56,8 @@ app.UseSwaggerUI(c =>
 MapperModel.SetMapper(app.Services.GetRequiredService<IMapper>());
 
 app.UseHttpsRedirection();
+
+app.UseCors("MayTheFourth");
 
 app.MapRoutes();
 
