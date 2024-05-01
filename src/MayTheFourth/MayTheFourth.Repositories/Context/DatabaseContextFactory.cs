6dce2cc762b1099adf13926d96181367be59a58c
Jonathan Vale <creativeweb.de@gmail.com>
2024-05-01 19:15:58 -0300

    Add docker support

--- src/MayTheFourth/MayTheFourth.Repositories/Context/DatabaseContextFactory.cs
+++ src/MayTheFourth/MayTheFourth.Repositories/Context/DatabaseContextFactory.cs
@@ -4,24 +4,37 @@ using Microsoft.Extensions.Configuration;
 
 namespace MayTheFourth.Repositories.Context
 {
-    internal class DatabaseContextFactory: IDesignTimeDbContextFactory<DatabaseContext>
+    public class DatabaseContextFactory: IDesignTimeDbContextFactory<DatabaseContext>
     {
         public DatabaseContext CreateDbContext(string[] args)
         {
             var optionsBuilder = new DbContextOptionsBuilder<DatabaseContext>();
 
-            var builder = new DbContextOptionsBuilder<DatabaseContext>();
+            var connectionString = args.Length > 0
+                ? args[0]
+                : "";
+            
+            if (args.Length <= 0)
+            {
+                var configuration = new ConfigurationBuilder()
+                    .SetBasePath(Directory.GetCurrentDirectory())
+                    .AddJsonFile("appsettings.json")
+                    .Build();
 
-            var configuration = new ConfigurationBuilder()
-                .SetBasePath(Directory.GetCurrentDirectory())
-                .AddJsonFile("appsettings.json")
-                .Build();
-
-            var connectionString = configuration.GetConnectionString(configuration["DefaultConnectionString"] ?? "");
+                connectionString = configuration.GetConnectionString(configuration["DefaultConnectionString"] ?? "");
+            }
 
             optionsBuilder.UseNpgsql(connectionString);
 
             return new DatabaseContext(optionsBuilder.Options);
+        }
+
+        public void Migrate(DatabaseContext context, bool clearDatabase)
+        {
+            if (clearDatabase)
+                context.Database.EnsureDeleted();
+            
+            context.Database.Migrate();
         }
     }
 }
